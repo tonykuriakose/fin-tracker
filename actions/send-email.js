e43fba70d0e59b2474480e73ac198bf6d79cd289
Tony <t4tonykuriakose@gmail.com>
2025-03-08 09:20:13 +0530

    email send

--- actions/send-email.js
+++ actions/send-email.js
@@ -2,6 +2,7 @@
 
 import { Resend } from "resend";
 
+// send email using resend email library
 export async function sendEmail({ to, subject, react }) {
   const resend = new Resend(process.env.RESEND_API_KEY || "");
 
